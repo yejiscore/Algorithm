66424fbb11ce9ac3adf8ab4a9d5a39833c4d4b84
yejiscore <80230766+yejiscore@users.noreply.github.com>
2024-08-12 01:24:35 +0900

    [Bronze III] Title: 공 넣기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.py
+++ 백준/Bronze/10810. 공 넣기/공 넣기.py
@@ -1,13 +1,17 @@
 n, m = list(map(int, input().split()))
 
+# n개의 바구니 공간을 만들어준다.
+# 공이 들어있지 않은 바구니는 0을 출력하라고 했으므로
+# 모든 바구니를 0으로 설정한다.
+# basket = [0, 0, 0, ... , 0]
 basket = []
 for _ in range(n):
-    basket.append(0)
+  basket.append(0)
 
 for _ in range(m):
-    a, b, c = list(map(int, input().split()))
+  i, j, k = map(int, input().split())
 
-    for i in range(a, b+1):
-        basket[i-1] = c
+  for x in range(i, j+1):
+    basket[x-1] = k
 
 print(*basket)
