476acad7a32c25ffc9d5f29321283eb5b2cdc354
yejiscore <80230766+yejiscore@users.noreply.github.com>
2024-08-15 01:40:17 +0900

    [Bronze II] Title: 소수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2581. 소수/소수.py
+++ 백준/Bronze/2581. 소수/소수.py
@@ -3,17 +3,16 @@ N = int(input())
 result = []
 
 for i in range(M, N+1):
-  if i < 2:
+    if i < 2:
         continue
-  for j in range(2, i + 1):
-    if i % j == 0:
-      if i == j:
+    for j in range(2, int(i**0.5) + 1):
+        if i % j == 0:
+            break
+    else:
         result.append(i)
-      break
 
 if result:
-  print(sum([x for x in result]))
-  print(min(result))
-  
+    print(sum(result))
+    print(min(result))
 else:
-  print(-1)
+    print(-1)
