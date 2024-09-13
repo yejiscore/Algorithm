023ca443ce91b48c127710197fc04cd921ed11ec
yejiscore <80230766+yejiscore@users.noreply.github.com>
2024-09-13 19:40:44 +0900

    [Bronze III] Title: 알람 시계, Time: 108 ms, Memory: 14500 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.java
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -1,28 +1,26 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class Main {
-  public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
+        // 입력
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int hour = Integer.parseInt(st.nextToken());
+        int minute = Integer.parseInt(st.nextToken());
+        
+        // 0분 미만이면 minute+60 hour-1
+        // hour 이 -1이라면 23으로 바꾸기
+        minute -= 45;
+        if (minute < 0) {
+            minute += 60;
+            hour -= 1;
+        }
+        if (hour == -1) hour = 23;
 
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
-    
-		int hour = Integer.parseInt(st.nextToken());
-		int minute = Integer.parseInt(st.nextToken());
-
-		if (minute >= 45) {
-			minute = minute - 45;
-			System.out.println(hour + " " + minute);
-		} else {
-			minute = minute + 15;
-			if (hour == 0) {
-				hour = 23;
-			} else {
-				hour = hour - 1;
-			}
-			System.out.println(hour + " " + minute);
-		}
-	}
+        System.out.printf("%d %d", hour, minute);
+    }
 }
