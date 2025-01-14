6b3e180c5545786afb499be4a089dfeed4a98bb6
yejiscore <80230766+yejiscore@users.noreply.github.com>
2025-01-14 21:18:57 +0900

    Title: 실수 입력받아 계산, Time: 151ms, Memory: 11MB, Status: Passed - CodeTree

--- 250114/실수 입력받아 계산/calculation-by-inputting-a-real-number.java
+++ 250114/실수 입력받아 계산/calculation-by-inputting-a-real-number.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+
+        System.out.printf("%.2f", (a+b));
     }
 }
