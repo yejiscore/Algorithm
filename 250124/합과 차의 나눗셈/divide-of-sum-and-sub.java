314c56d5577964dd1b1606382128c738782ed4c8
yejiscore <80230766+yejiscore@users.noreply.github.com>
2025-01-24 18:30:21 +0900

    Title: 합과 차의 나눗셈, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250124/합과 차의 나눗셈/divide-of-sum-and-sub.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        double result = (a+b)/(double)(a-b);
+        
+        System.out.printf("%.2f", result);
+    }
+}
