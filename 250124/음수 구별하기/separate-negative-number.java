686de90f72f112c243ebaab8b9c854b653609d10
yejiscore <80230766+yejiscore@users.noreply.github.com>
2025-01-24 18:44:11 +0900

    Title: 음수 구별하기, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

--- 250124/음수 구별하기/separate-negative-number.java
+++ 250124/음수 구별하기/separate-negative-number.java
@@ -4,12 +4,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+
+        System.out.println(n);
         
         if (n < 0) {
-            System.out.println(n);
             System.out.println("minus");
-        } else {
-            System.out.println(n);
         }
     }
 }
