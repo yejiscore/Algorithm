89b2b54371d7e8d339e0ba2108d89825090d5ddc
yejiscore <80230766+yejiscore@users.noreply.github.com>
2025-01-24 18:34:38 +0900

    Title: 합과 평균, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250124/합과 평균/sum-and-avg.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        int sum = a+b;
+        double avg = (double) sum / 2;
+
+        System.out.print(sum + " " + avg);
+    }
+}
