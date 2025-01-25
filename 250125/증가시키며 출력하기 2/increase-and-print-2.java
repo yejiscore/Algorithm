367d3eeec2cc5b06868f3f518a41560cf13fa897
yejiscore <80230766+yejiscore@users.noreply.github.com>
2025-01-25 23:14:06 +0900

    Title: 증가시키며 출력하기 2, Time: 120ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250125/증가시키며 출력하기 2/increase-and-print-2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int a = 10;
+
+        while (a <= 26) {
+            System.out.print(a + " ");
+            a++;
+        }
+    }
+}
