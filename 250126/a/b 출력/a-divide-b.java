a1e9e70d1e72d99ca876513f1847aa38fb7f010e
yejiscore <80230766+yejiscore@users.noreply.github.com>
2025-01-26 00:41:07 +0900

    Title: a/b 출력, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

--- 250126/a/b 출력/a-divide-b.java
+++ 250126/a/b 출력/a-divide-b.java
@@ -3,12 +3,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-        double result = (double) a / b;
-
-        System.out.printf("%.20f", result);
+        // 몫 출력
+        System.out.print(a / b + ".");
+        
+        // 소수점 출력
+        a %= b;
+        for(int i = 0; i < 20; i++) {
+            a *= 10;
+            System.out.print(a / b);
+            a %= b;
+        }
     }
 }
