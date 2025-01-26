dd4eb596ff6404b8c9bcbd6df9534cd2816e30b5
yejiscore <80230766+yejiscore@users.noreply.github.com>
2025-01-26 20:17:05 +0900

    Title: 조건에 부합하는 수, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250126/조건에 부합하는 수/number-that-meets-the-condition.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        for (int i = 1; i <= a; i++) {
+            if ((i % 2 == 0 && i % 4 != 0) || (i / 8) % 2 == 0 || i % 7 < 4) {
+                continue;
+            } else {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
