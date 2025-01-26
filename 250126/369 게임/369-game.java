81df493f79e3adb08fc25dcb19ad4259640457a7
yejiscore <80230766+yejiscore@users.noreply.github.com>
2025-01-26 19:31:40 +0900

    Title: 369 게임, Time: 120ms, Memory: 10MB, Status: Passed - CodeTree

--- 250126/369 게임/369-game.java
+++ 250126/369 게임/369-game.java
@@ -7,7 +7,7 @@ public class Main {
         int n = sc.nextInt();
 
         for (int i = 1; i <= n; i++) {
-            if (i % 3 == 0 || i % 10 == 3 || i / 10 == 3 || i / 10 == 6 || i / 10 == 9) {
+            if (i % 3 == 0 || i % 10 == 3 || i % 10 == 6 || i % 10 == 9 || i / 10 == 3 || i / 10 == 6 || i / 10 == 9) {
                 System.out.print(0 + " ");
             } else {
                 System.out.print(i + " ");
