f8367f2b27ccf4e6e1750d74911f26f6e2039584
yejiscore <80230766+yejiscore@users.noreply.github.com>
2025-05-23 22:00:46 +0900

    [level 0] Title: 배열 자르기, Time: 0.03 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int[] numbers, int num1, int num2) {
+        int[] answer = new int[num2 - num1 + 1];
+        
+        for (int i = num1, j = 0; i <= num2; i++, j++) {
+            answer[j] = numbers[i];
+        }
+        
+        return answer;
+    }
+}
