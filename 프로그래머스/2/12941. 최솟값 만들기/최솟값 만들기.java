5541cb2093125bd6bc6731299ddecab704f85ba5
yejiscore <80230766+yejiscore@users.noreply.github.com>
2025-05-31 16:04:46 +0900

    [level 2] Title: 최솟값 만들기, Time: 2.34 ms, Memory: 54.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class Solution
+{
+    public int solution(int []A, int []B)
+    {
+        Arrays.sort(A);
+        Arrays.sort(B);
+        int n = A.length;
+        int sum = 0;
+        
+        for (int i = 0; i < n; i++) {
+            sum += A[i] * B[n-i-1];
+        }
+        
+        return sum;
+    }
+}
