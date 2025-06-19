30fac3a8fcc4a87720cac1248cd1f52f77577293
yejiscore <80230766+yejiscore@users.noreply.github.com>
2025-06-19 13:39:20 +0900

    [level 0] Title: 할 일 목록, Time: 0.06 ms, Memory: 87.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        
+        List<String> result = new ArrayList<>();
+        
+        for (int i = 0;  i < todo_list.length; i++) {
+            if (!finished[i]) {
+                result.add(todo_list[i]);
+            }
+        }
+        
+        String[] answer = result.toArray(new String[0]);
+        
+        return answer;
+    }
+}
