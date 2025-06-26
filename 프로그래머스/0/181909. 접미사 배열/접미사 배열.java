56cfeeadd306c733f1d8aa3afab524a2e9176372
yejiscore <80230766+yejiscore@users.noreply.github.com>
2025-06-26 22:33:44 +0900

    [level 0] Title: 접미사 배열, Time: 0.50 ms, Memory: 87.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String my_string) {
+        int len = my_string.length();
+        String[] suffixes = new String[len];
+        
+        for (int i = 0; i < len; i++) {
+            suffixes[i] = my_string.substring(i);
+        }
+        
+        Arrays.sort(suffixes);
+        return suffixes;
+    }
+}
