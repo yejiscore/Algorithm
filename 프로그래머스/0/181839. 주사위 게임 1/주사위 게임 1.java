46a602990ed186d87bdfe4ac8177f637e1e0962d
yejiscore <80230766+yejiscore@users.noreply.github.com>
2026-02-19 23:37:00 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.05 ms, Memory: 88.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int a, int b) {
+        
+        boolean aOdd = (a % 2 == 1);
+        boolean bOdd = (b % 2 == 1);
+        
+        if (aOdd && bOdd) {
+            return a * a + b * b;
+        } else if (aOdd || bOdd) {
+            return 2 * (a + b);
+        } else {
+            return Math.abs(a - b);
+        }
+    }
+}
