50a9f7216ba7dc873288b33a20dd1a2eb4ea86bf
yejiscore <80230766+yejiscore@users.noreply.github.com>
2026-03-02 23:35:13 +0900

    [level 0] Title: ad 제거하기, Time: 0.91 ms, Memory: 84.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+class Solution {
+    public String[] solution(String[] strArr) {
+        ArrayList<String> list = new ArrayList<>();
+
+        for (String str : strArr) {
+            if (!str.contains("ad")) {
+                list.add(str);
+            }
+        }
+
+        return list.toArray(new String[0]);
+    }
+}
