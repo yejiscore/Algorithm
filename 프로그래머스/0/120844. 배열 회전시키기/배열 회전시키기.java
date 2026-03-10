03c349ef1b675454a78402f6bd121dbb4e914f36
yejiscore <80230766+yejiscore@users.noreply.github.com>
2026-03-10 11:10:33 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.03 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] solution(int[] numbers, String direction) {
+        
+        int n = numbers.length;
+        int[] answer = new int[n];
+        
+        if (direction.equals("right")) {
+            answer[0] = numbers[n-1];
+            for (int i = 0; i < n-1; i++) {
+                answer[i+1] = numbers[i];
+            }
+        } else {
+            answer[n-1] = numbers[0];
+            for (int i = 1; i < n; i++) {
+                answer[i-1] = numbers[i];
+            }
+        }
+        
+        return answer;
+    }
+}
