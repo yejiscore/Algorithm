63eb35681db43809393cfc413469edefbaeb5e9f
yejiscore <80230766+yejiscore@users.noreply.github.com>
2026-03-10 11:03:29 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.02 ms, Memory: 85.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        int pizza = 1;
+        
+        while (pizza*6 % n > 0) {
+            if (pizza*6 % n != 0) {
+                pizza++;
+            }
+        }
+        
+        return pizza;
+    }
+}
