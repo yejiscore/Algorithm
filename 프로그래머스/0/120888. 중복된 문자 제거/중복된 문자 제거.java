3c95358eb04ff612e98bffb303f80c4169efc396
yejiscore <80230766+yejiscore@users.noreply.github.com>
2026-03-13 23:47:54 +0900

    [level 0] Title: 중복된 문자 제거, Time: 12.95 ms, Memory: 84.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        
+        for (int i = 0; i < my_string.length(); i++) {
+            char c = my_string.charAt(i);
+            
+            if (!answer.contains(String.valueOf(c))) {
+                answer += c;
+            }
+        }
+        
+        return answer;
+    }
+}
