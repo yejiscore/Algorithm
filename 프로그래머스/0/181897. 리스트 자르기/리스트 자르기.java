2adf0fab3ad25b0ae6ec362941e4469cc7ac8a6d
yejiscore <80230766+yejiscore@users.noreply.github.com>
2026-04-11 01:32:55 +0900

    [level 0] Title: 리스트 자르기, Time: 2.72 ms, Memory: 90.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, int[] slicer, int[] num_list) {
+        
+        int a = slicer[0];
+        int b = slicer[1];
+        int c = slicer[2];
+        
+        List<Integer> list = new ArrayList<>();
+        
+        if (n == 1) {
+            for (int i = 0; i <= b; i++) {
+                list.add(num_list[i]);
+            }
+        }
+        
+        else if (n == 2) {
+            for(int i = a; i < num_list.length; i++){
+                list.add(num_list[i]);
+            }
+        }
+        
+        else if (n == 3){
+            for(int i = a; i <= b; i++){
+                list.add(num_list[i]);
+            }
+        }
+        
+        else if (n == 4) {
+            for (int i = a; i <= b; i += c) {
+                list.add(num_list[i]);
+            }
+        }
+        
+        return list.stream().mapToInt(i -> i).toArray();
+    }
+}
